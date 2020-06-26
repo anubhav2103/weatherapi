8d1bd372937bc6eea94eb61083c1f2fb8a1fec1c
Anubhuv Kumar <100expertdays@gmail.com>
2020-06-26 17:47:50 +0530

    Updates

---
+++ precache-manifest.9d4f909963b432bac27c5106a189f459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f53907eb3a980092ba464de90f0037",
+    "url": "/weatherapi/index.html"
+  },
+  {
+    "revision": "6e5d4eb119cebcedfec7",
+    "url": "/weatherapi/static/css/main.c1e46d96.chunk.css"
+  },
+  {
+    "revision": "b403904f101a1d2590d0",
+    "url": "/weatherapi/static/js/2.ab950d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapi/static/js/2.ab950d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5d4eb119cebcedfec7",
+    "url": "/weatherapi/static/js/main.e590ac22.chunk.js"
+  },
+  {
+    "revision": "6f0a74abffb36f69cbb8",
+    "url": "/weatherapi/static/js/runtime-main.f2947c80.js"
+  }
+]);
